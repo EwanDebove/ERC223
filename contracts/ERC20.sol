3e31bc77865697ebc398e5993ec4faa149434896
EwanDebove <ewan.debove@gmail.com>
2019-11-20 12:23:32 +0100

    oui

---
+++ contracts/ERC20.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyERC721 is ERC20 {
+  
+}
---
+++ contracts/ERC223.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.0;
+
+import "./ERC223/ERC223.sol";
+
+contract MyERC223 is IERC223 {
+  
+}
